b6ef1d50ceb4080713ef3146f2265f9514ad97ce
jeka <eu-1982@yandex.ru>
2022-11-15 10:56:32 +0700

    Initial Commit

---
+++ src/main/java/ru/gb/worktaskmanager/worktaskmanager/WorktaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.worktaskmanager.worktaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/worktaskmanager/worktaskmanager/WorktaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.worktaskmanager.worktaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
