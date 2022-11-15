b62783a99a4088cd1b2c7735d013b241beab1b02
jeka <eu-1982@yandex.ru>
2022-11-15 11:31:45 +0700

    develop branch created some springboot changes in main files

---
+++ managercore/src/main/java/ru/gb/worktaskmanager/MainManagerCoreApplication.java
@@ -0,0 +1,10 @@
+package ru.gb.worktaskmanager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainManagerCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainManagerCoreApplication.class, args);
+    }
+}
