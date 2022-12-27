c59a36486da14883b6fe134e780704b9b6feee61
jeka <eu-1982@yandex.ru>
2022-12-27 18:48:01 +0700

    all tasks and new task

--- managerfront/src/main/resources/static/newtask/newtask.js
+++ managerfront/src/main/resources/static/newtask/newtask.js
@@ -1,21 +1,11 @@
 angular.module('taskmanager').controller('newTaskController', function ($scope, $rootScope, $http) {
-    $scope.newTask = {
-        title: "",
-        description: "",
-        employerId: 0,
-        authorId: 2,
-        responsibleUserId: 0,
-        workingHours: 0,
-        planStartDate: "2022-12-28T04:49:00.000Z",
-        planEndDate: "2022-12-29T04:49:00.000Z"
-    };
 
     $scope.createNewTask = function () {
         $scope.newTask.workingHours = ($scope.newTask.planEndDate - $scope.newTask.planStartDate) / 3600000;
         $http({
             url: 'http://localhost:5555/core/tasks',
-            method: 'POST',
-            body: $scope.newTask
+            body: $scope.newTask,
+            method: 'POST'
         }).then(function (response) {
 
             });
