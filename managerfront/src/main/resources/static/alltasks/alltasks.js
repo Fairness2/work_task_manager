131a2cb1f90ff918ebde41aaa02ed0db60efa897
jeka <eu-1982@yandex.ru>
2022-12-27 21:26:41 +0700

    all tasks backend and frontend fixes

--- managerfront/src/main/resources/static/alltasks/alltasks.js
+++ managerfront/src/main/resources/static/alltasks/alltasks.js
@@ -1,16 +1,18 @@
 angular.module('taskmanager').controller('allTasksController', function ($scope, $http, $localStorage) {
 
     $scope.loadTasks = function (pageIndex = 1) {
-        $scope.tasksRequest = {
-            page: pageIndex,
-            userId: $localStorage.workTaskUser.id
+
+            $http({
+                url: 'http://localhost:5555/core/tasks/get-all',
+                method: 'GET',
+                params: {
+                    pageIndex: pageIndex
+                }
+            }).then(function (response) {
+                $scope.allTasks = response.data;
+                $scope.generatePagesList($scope.allTasks.total);
+            });
         };
-        $http.get('http://localhost:5555/core/tasks', $scope.tasksRequest)
-            .then(function (response) {
-            $scope.allTasks = response.data;
-            $scope.generatePagesList($scope.allTasks.total);
-        });
-    };
 
     $scope.generatePagesList = function (total) {
         out = [];
